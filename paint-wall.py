9934303d28389c2c43120d07bd0e7f37c8a74e75
Ammon Shepherd <ammon.shepherd@gmail.com>
2024-06-07 10:57:55 -0400

    Week 4. Problem 3: Paint wall. Step 1

---
+++ paint-wall.py
@@ -0,0 +1,10 @@
+from math import ceil
+
+wall_height = float(input("Wall height: "))
+wall_width = float(input("Wall width: "))
+
+cost_paint_can = float(input("Cost of one paint can: "))
+
+wall_area = wall_height * wall_width
+
+print(f"Wall area: {wall_area:.1f} sq ft")
