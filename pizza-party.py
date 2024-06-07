993f11a9fa16348c2304c03c756e19bd691e1d94
Ammon Shepherd <ammon.shepherd@gmail.com>
2024-06-07 10:09:41 -0400

    Week 4. Problem 2. Step 2

--- pizza-party.py
+++ pizza-party.py
@@ -14,7 +14,11 @@ pizzas_needed = math.ceil(slices_needed / slices_per_pizza)
 
 pizza_cost = one_pizza_cost * pizzas_needed
 
+sales_tax = pizza_cost * 0.07
+delivery_charge = (pizza_cost + sales_tax) * 0.2
+
 print()
 print("Friday Night Party")
 print(f"{pizzas_needed} Pizzas: ${pizza_cost:.2f}")
-
+print(f"Tax: ${sales_tax:.2f}")
+print(f"Delivery: ${delivery_charge:.2f}")
