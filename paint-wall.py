d6bd5b5866964295b8b3cfb558cb10cf64ed337e
Ammon Shepherd <ammon.shepherd@gmail.com>
2024-06-07 10:59:08 -0400

    Week 4. Problem 3: Paint Wall. Step 2

--- paint-wall.py
+++ paint-wall.py
@@ -7,4 +7,9 @@ cost_paint_can = float(input("Cost of one paint can: "))
 
 wall_area = wall_height * wall_width
 
+paint_coverage = 350 # one gallon of paint covers 350 square feet
+
+paint_needed = wall_area / paint_coverage
+
 print(f"Wall area: {wall_area:.1f} sq ft")
+print(f"Paint needed: {paint_needed:.3f} gallons")
