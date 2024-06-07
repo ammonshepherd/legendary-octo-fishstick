6db708995ff9f191e1cb00e7e44107ab33dc2e79
Ammon Shepherd <ammon.shepherd@gmail.com>
2024-06-07 11:04:08 -0400

    Week 4. Problem 3: Paint Wall. Step 4

--- paint-wall.py
+++ paint-wall.py
@@ -13,6 +13,13 @@ paint_needed = wall_area / paint_coverage
 
 cans_needed = ceil(paint_needed)
 
+paint_cost = cans_needed * cost_paint_can
+sales_tax = paint_cost * .07
+total_cost = paint_cost + sales_tax
+
 print(f"Wall area: {wall_area:.1f} sq ft")
 print(f"Paint needed: {paint_needed:.3f} gallons")
 print(f"Cans needed: {cans_needed} can(s)")
+print(f"Paint cost: ${paint_cost:.2f}")
+print(f"Sales tax: ${sales_tax:.2f}")
+print(f"Total cost: ${total_cost:.2f}")
